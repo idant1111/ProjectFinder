d68de2993ec17124a0a7047f66097424a2e74e68
Idan Teigfeld <idant1111@users.noreply.github.com>
2024-08-17 13:43:51 +0300

    added long desc to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='projectfinder',
     version='0.1',
     packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
         'click',
         'rich',
